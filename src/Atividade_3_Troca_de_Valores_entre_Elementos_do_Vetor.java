41440c0f964c8c9352eac900b87320c2d0ffd93a
João Pedro G U <41847866+Charmandercharmandesco@users.noreply.github.com>
2024-09-04 20:27:31 -0300

    Inicializar um vetor com 4 posições e adicionando os elementos 3,6,9 e 12 neles. Exibe a soma da posição 1 coma a posição 4.

---
+++ src/Atividade_3_Troca_de_Valores_entre_Elementos_do_Vetor.java
@@ -0,0 +1,15 @@
+public class Atividade_3_Troca_de_Valores_entre_Elementos_do_Vetor {
+    public static void main(String[] args) {
+
+        int[] taMuitoFacilAsAtividadesCleiton = new int[2];
+
+        taMuitoFacilAsAtividadesCleiton[0] = 5;
+        taMuitoFacilAsAtividadesCleiton[1] = 10;
+
+        taMuitoFacilAsAtividadesCleiton[0] = 10;
+        taMuitoFacilAsAtividadesCleiton[1] = 5;
+
+        System.out.println(taMuitoFacilAsAtividadesCleiton[0] + " " + taMuitoFacilAsAtividadesCleiton[1]);
+
+    }
+}
