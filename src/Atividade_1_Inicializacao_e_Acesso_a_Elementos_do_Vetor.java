81f84b379557aa62eb904f63151309c9db664dae
João Pedro G U <41847866+Charmandercharmandesco@users.noreply.github.com>
2024-09-04 20:09:40 -0300

    Inicializando um vetor com 5 posições e adicionando elementos nelas. Exibe a terceira posição.

---
+++ src/Atividade_1_Inicializacao_e_Acesso_a_Elementos_do_Vetor.java
@@ -0,0 +1,13 @@
+public class Atividade_1_Inicializacao_e_Acesso_a_Elementos_do_Vetor {
+    public static void main(String[] args) {
+
+        int[] vetor = new int[5];
+        vetor[0] = 10;
+        vetor[1] = 20;
+        vetor[2] = 30;
+        vetor[3] = 40;
+        vetor[4] = 50;
+
+        System.out.println(vetor[2]);
+    }
+}
