e50f9385330bc68e6ed9299749038a2d7ba0857d
João Pedro G U <41847866+Charmandercharmandesco@users.noreply.github.com>
2024-09-04 20:18:52 -0300

    Inicializar um vetor com 4 posições e adicionando os elementos 3,6,9 e 12 neles. Exibe a soma da posição 1 coma a posição 4.

---
+++ src/Atividade_2_Operacoes_Simples_com_Elementos_do_Vetor.java
@@ -0,0 +1,12 @@
+public class Atividade_2_Operacoes_Simples_com_Elementos_do_Vetor {
+    public static void main(String[] args) {
+
+        int[] socorroCleiton  = new int[4];
+        socorroCleiton[0] = 3;
+        socorroCleiton[1] = 6;
+        socorroCleiton[2] = 9;
+        socorroCleiton[3] = 12;
+
+        System.out.println(socorroCleiton[0]+socorroCleiton[3]);
+    }
+}
