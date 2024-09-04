1247f7f734a6afeec388c25ab5b2fea43b4a9a90
João Pedro G U <41847866+Charmandercharmandesco@users.noreply.github.com>
2024-09-04 20:51:28 -0300

    Inicializar um vetor com 3 posições e pede para o usuário colocar três números em cada posição. Depois exibe as três posições e faz uma média delas.

---
+++ src/Atividade_4_Leitura_de_Valores_pelo_Usuario.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Atividade_4_Leitura_de_Valores_pelo_Usuario {
+    public static void main(String[] args) {
+        int[] fazPeloMenos10AtividadesEUmasDuasMaisDificeisCleiton = new[3];
+
+        Scanner cheirador = new Scanner(System.in);
+
+        System.out.println("Irei precisar de três números\n" + "Me dê o primeiro número: ");
+        fazPeloMenos10AtividadesEUmasDuasMaisDificeisCleiton [0] = cheirador.nextInt();
+        System.out.println("Me dê o segundo número: ");
+        fazPeloMenos10AtividadesEUmasDuasMaisDificeisCleiton [1] = cheirador.nextInt();
+        System.out.println("Me dê o terceiro número: ");
+        fazPeloMenos10AtividadesEUmasDuasMaisDificeisCleiton [2] = cheirador.nextInt();
+
+
+    }
+}
