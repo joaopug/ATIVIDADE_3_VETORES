0875ee6db926abfae797eff076d85b3dd7a87476
joaopug <charmandercharmandesco@gmail.com>
2024-09-06 20:01:22 -0300

    Melhorando o código

--- src/Atividade_3_Troca_de_Valores_entre_Elementos_do_Vetor.java
+++ src/Atividade_3_Troca_de_Valores_entre_Elementos_do_Vetor.java
@@ -6,11 +6,12 @@ public class Atividade_3_Troca_de_Valores_entre_Elementos_do_Vetor {
         taMuitoFacilAsAtividadesCleiton[0] = 5;
         taMuitoFacilAsAtividadesCleiton[1] = 10;
 
+        System.out.println("Os valores são: \n" + taMuitoFacilAsAtividadesCleiton[0] + " e " + taMuitoFacilAsAtividadesCleiton[1]);
+
         taMuitoFacilAsAtividadesCleiton[0] = 10;
         taMuitoFacilAsAtividadesCleiton[1] = 5;
 
-
-        System.out.println(taMuitoFacilAsAtividadesCleiton[0] + " " + taMuitoFacilAsAtividadesCleiton[1]);
+        System.out.println("Com a troca feita, os resultados ficam assim: \n" + taMuitoFacilAsAtividadesCleiton[0] + " e " + taMuitoFacilAsAtividadesCleiton[1]);
 
     }
 }
