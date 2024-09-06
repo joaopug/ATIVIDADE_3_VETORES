2404000ca7ebf62790ec4104f46b678e4184cc6c
joaopug <charmandercharmandesco@gmail.com>
2024-09-06 20:09:58 -0300

    Pede o input de três números e guarda dentro de um vetor com também três posições. Após isso exibe os números e a média deles.

--- src/Atividade_4_Leitura_de_Valores_pelo_Usuario.java
+++ src/Atividade_4_Leitura_de_Valores_pelo_Usuario.java
@@ -2,17 +2,20 @@ import java.util.Scanner;
 
 public class Atividade_4_Leitura_de_Valores_pelo_Usuario {
     public static void main(String[] args) {
-        int[] fazPeloMenos10AtividadesEUmasDuasMaisDificeisCleiton = new[3];
+        int[] fazPeloMenos10AtividadesEUmasDuasMaisDificeisCleiton = new int[3];
 
         Scanner cheirador = new Scanner(System.in);
 
         System.out.println("Irei precisar de três números\n" + "Me dê o primeiro número: ");
-        fazPeloMenos10AtividadesEUmasDuasMaisDificeisCleiton [0] = cheirador.nextInt();
+        fazPeloMenos10AtividadesEUmasDuasMaisDificeisCleiton[0] = cheirador.nextInt();
         System.out.println("Me dê o segundo número: ");
-        fazPeloMenos10AtividadesEUmasDuasMaisDificeisCleiton [1] = cheirador.nextInt();
+        fazPeloMenos10AtividadesEUmasDuasMaisDificeisCleiton[1] = cheirador.nextInt();
         System.out.println("Me dê o terceiro número: ");
-        fazPeloMenos10AtividadesEUmasDuasMaisDificeisCleiton [2] = cheirador.nextInt();
+        fazPeloMenos10AtividadesEUmasDuasMaisDificeisCleiton[2] = cheirador.nextInt();
 
+        int seVoceFizerMaisAtividadesCleitonDaPraEstudarMelhor = (fazPeloMenos10AtividadesEUmasDuasMaisDificeisCleiton[0] + fazPeloMenos10AtividadesEUmasDuasMaisDificeisCleiton[1] + fazPeloMenos10AtividadesEUmasDuasMaisDificeisCleiton[2]) / 3;
+
+        System.out.println("O primeiro número que você digitou: " + fazPeloMenos10AtividadesEUmasDuasMaisDificeisCleiton[0] + "\n" + "O segudo número que você digitou: " + fazPeloMenos10AtividadesEUmasDuasMaisDificeisCleiton[1] + "\n" + "O terceiro número que você digitou: " + fazPeloMenos10AtividadesEUmasDuasMaisDificeisCleiton[2] + "\n" + "Se juntarmos e fizermos uma média com eles, este será o resultado: " + seVoceFizerMaisAtividadesCleitonDaPraEstudarMelhor);
 
     }
 }
